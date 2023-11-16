0ca0440e8c58f6e234694df9b03c3db8ad748062
Ahmadou Ngary Faye <ngaryfaye95@gmail.com>
2023-11-16 12:41:58 +0000

    Active link style

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+"use client";
+
+import React, { use } from "react";
 import Link from "next/link";
 import { AiFillBug } from "react-icons/ai";
+import { usePathname } from "next/navigation";
+import classnames from "classnames";
 
 const links = [
   {
@@ -12,7 +16,10 @@ const links = [
     href: "/issues",
   },
 ];
+
 const Navbar = () => {
+  const currentPath = usePathname();
+
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">
@@ -23,7 +30,11 @@ const Navbar = () => {
           <li>
             <Link
               href={link.href}
-              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+              className={classnames({
+                "text-zinc-900": currentPath === link.href,
+                "text-zinc-500": currentPath !== link.href,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
             >
               {link.label}
             </Link>
